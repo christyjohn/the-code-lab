a9cd99f9211f65f336bcdb7d534cbb9ce2c000c0
Christy John <christyjohn.crz@gmail.com>
2026-06-13 18:47:42 +0530

    Initialized project

---
+++ springboot-kafka-demo/README.md
@@ -0,0 +1,2 @@
+# Spring Boot Kafka Project
+Learning Kafka with Spring Boot using [Spring Boot Kafka Full Course in 3 Hours](https://www.youtube.com/watch?v=inrQUHLPFd4)
---
+++ springboot-kafka-demo/src/test/java/io/github/christyjohn/springboot/SpringbootKafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.christyjohn.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
