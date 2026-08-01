c1da2e8dbec8e0af5ede4a69e82c289cbaa66cb9
Christy John <christyjohn.crz@gmail.com>
2026-08-01 23:57:32 +0530

    First 3 in Blind 75

---
+++ DSA_Practice/src/com/christyjohn/destination_faang/leetcode/blind75/P02_LC_121_BestTimeToBuySellStocks.java
@@ -0,0 +1,24 @@
+package com.christyjohn.destination_faang.leetcode.blind75;
+
+public class P02_LC_121_BestTimeToBuySellStocks {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+        System.out.println(maxProfit(new int[] { 1, 2 }));
+        System.out.println(maxProfit(new int[] { 7, 1, 5, 3, 6, 4, 9, 34 }));
+        System.out.println(maxProfit(new int[] { 7, 3, 5, 103, 6, 4, 9, 34, 206, 1, 205 }));
+        System.out.println(maxProfit(new int[] { 7, 6, 4, 3, 1 }));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy)
+                buy = prices[i];
+
+            profit = Math.max(profit, prices[i] - buy);
+        }
+        return profit;
+    }
+}
---
+++ DSA_Practice/src/com/christyjohn/destination_faang/leetcode/blind75/P03_LC_217_ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package com.christyjohn.destination_faang.leetcode.blind75;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P03_LC_217_ContainsDuplicate {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[] {1,2,3,1}));
+        System.out.println(containsDuplicate(new int[] {1,2,3,4}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num))
+                return true;
+            else
+                set.add(num);
+        }
+
+        return false;
+    }
+}
